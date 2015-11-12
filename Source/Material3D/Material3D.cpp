f94ac2d5cc6902471c7eb519fdcad2ff0db20904
Garrett Stevens <garrett.j.stevens@intel.com>
2015-11-11 17:12:48 -0800

    First commit. :P

---
+++ Source/Material3D/Material3D.cpp
@@ -0,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Material3D.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Material3D, "Material3D" );
---
+++ Source/Material3D/Material3DGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "Material3DGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MATERIAL3D_API AMaterial3DGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
